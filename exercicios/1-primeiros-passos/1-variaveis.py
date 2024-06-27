49fdac1d43ff81612b66a0df21801d66999a6054
luiza-dantasq <luiza.dantasq@gmail.com>
2024-06-27 22:39:10 +0000

    1-variaveis_feito

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,17 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Luiza'
+profissão = "Farmacêutica"
+objetivo = '''
+... aprender pyhton
+... para decidir meu futuro
+...'''
 # Declare variáveis de cada tipo numérico
-
+numero_inteiro = 27
+salario = 5500.00
+complexo = 2+1j
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
